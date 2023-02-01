34d2b06db51c8dfcfbec547ecfde926992fba313
Oshani Kumarathunga <100805282+OshaniKumarathunga@users.noreply.github.com>
2023-02-01 23:14:43 +0530

    background pictures added and updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,12 @@ import { DoctorComponent } from './Modules/Doctor/doctor landing page/doctor.com
 import { DoctorSignupComponent } from './Modules/Doctor/doctor-signup-page-1/doctor-signup.component';
 import { DoctorSignupNextComponent } from './Modules/Doctor/doctor-signup-page-2/doctor-signup-next.component';
 import { LoginComponent } from './Shared/login/login.component';
+import { HomeComponent } from './Shared/home/home.component';
 
 
 // routes for the components
 const routes: Routes = [
-  // {path: 'home', component:},
+  {path: 'home', component:HomeComponent},
   {path: 'doctor' , component:DoctorComponent},
   {path:'signup' ,component:DoctorSignupComponent },
   {path: 'signupnext' , component:DoctorSignupNextComponent},
