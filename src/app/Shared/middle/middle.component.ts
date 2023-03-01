a7c4326a65d18774df46b28aefd9deaaeec96209
Oshani Kumarathunga <100805282+OshaniKumarathunga@users.noreply.github.com>
2023-03-01 12:19:46 +0530

    adding aos

--- src/app/Shared/middle/middle.component.ts
+++ src/app/Shared/middle/middle.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as AOS from 'aos';
+import * as  AOS from 'aos';
 
 
 @Component({
@@ -14,5 +14,6 @@ export class MiddleComponent implements OnInit {
   ngOnInit(): void  {
     AOS.init({disable: 'mobile'});
     }
+    
 
 }
