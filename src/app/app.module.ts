60ca2a2d968c97f5d3abbac5bbfa28c2352b2dfe
Oshani Kumarathunga <oshanipbkonline@gmail.com>
2023-03-16 15:39:59 +0530

    ngx-owl-carousel-o@7.0.0 module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
 
 // import flexLayout
 import {FlexLayoutModule} from '@angular/flex-layout';
@@ -143,6 +145,7 @@ import { PharmacySignupPageComponent } from './Modules/Pharmacy/pharmacy-signup-
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    CarouselModule,
     FlexLayoutModule,
     MaterialModule,
     FormsModule,
