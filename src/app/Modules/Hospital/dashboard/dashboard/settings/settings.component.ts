871324a5b1b420fb81bd99a80936e70fef1e146c
Oshani Kumarathunga <oshanipbkonline@gmail.com>
2023-03-23 13:43:53 +0530

    hospital dashboard updated

--- src/app/Modules/Hospital/dashboard/dashboard/settings/settings.component.ts
+++ src/app/Modules/Hospital/dashboard/dashboard/settings/settings.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SettingsComponent implements OnInit {
 
-  
   constructor() { }
 
   ngOnInit(): void {
